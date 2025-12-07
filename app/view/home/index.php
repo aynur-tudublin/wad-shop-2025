dc854ca7d127a5ce5dc03a19fb24a9878fbda389
Aynur Hasanova <b00177512@mytudublin.ie>
2025-12-07 16:14:16 +0000

    Add welcome section to home page

---
+++ app/view/home/index.php
@@ -0,0 +1,4 @@
+<section class="card">
+    <h2 class="card__title">Welcome to the Grocery Store</h2>
+    <p>Browse grocery products, add them to your cart, and login to manage users.</p>
+</section>
