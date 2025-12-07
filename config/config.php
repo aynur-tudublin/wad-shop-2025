0209ae414b061bdd6861daa15621d7ca36c53db7
Aynur Hasanova <b00177512@mytudublin.ie>
2025-12-07 15:51:17 +0000

    Add database and path configuration settings

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+// from my local MAMP outputs
+define('DB_HOST', 'localhost');
+define('DB_PORT', '8889');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'wadshop2025');
+
+// file paths
+define('BASE_PATH', dirname(__DIR__));              // /.../wad-shop-2025
+define('PUBLIC_PATH', BASE_PATH . '/public');
+
+// it returns the folder name in the URL, e.g. /wad-shop-2025/public
+define('BASE_URL', '/wad-shop-2025/public');
