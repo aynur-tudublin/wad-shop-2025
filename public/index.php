dd552a61dffe60f9d90324fee148e45afb29b14b
Aynur Hasanova <b00177512@mytudublin.ie>
2025-12-07 15:57:19 +0000

    Add helper functions inclusion in index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../core/helpers.php';
 
 // selects controller and action
 $controller = isset($_GET['controller']) ? $_GET['controller'] : 'home';
